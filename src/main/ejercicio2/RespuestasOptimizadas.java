31ad6ecd204d1d1385218b97cd0eb12e900ec804
MJMMiller <mateomiller1605@gmail.com>
2025-05-21 07:31:31 -0500

    FIX(UPS-002): Se corrijieron errores y se completo ejercicio2

--- src/main/ejercicio2/RespuestasOptimizadas.java
+++ src/main/ejercicio2/RespuestasOptimizadas.java
@@ -1,5 +1,5 @@
 package main.ejercicio2;
-
+import java.util.HashSet;
 import java.util.Set;
 
 public class RespuestasOptimizadas {
@@ -21,22 +21,22 @@ public class RespuestasOptimizadas {
     }
 
     // 2. Versión optimizada de contarMayores (Complejidad anotada en la variable)
-    public static String contarMayoresComplejidad = ""; // Ej: "O(n)"
+    public static String contarMayoresComplejidad = "O(n)"; // Ej: "O(n)"
 
     public static int contarMayoresOpt(int[] arr, int x) {
-        // TODO: implementar versión eficiente
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            boolean esMayor = true;
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[i])
-                    esMayor = false;
+        int maxAnterior = Integer.MIN_VALUE;
+        HashSet<Integer> mayores = new HashSet<>();
+
+        for (int num : arr) {
+            if (num > maxAnterior) {
+                maxAnterior = num;
+                if (num > x) {
+                    mayores.add(num);
+                }
             }
-            if (arr[i] > x && esMayor)
-                count++;
         }
-        return count;
-    }
+        return mayores.size();
+}
 
     // 3. Versión optimizada de encontrarMaximo (Complejidad anotada en la variable)
     public static String encontrarMaximoComplejidad = ""; // Ej: "O(n)"
